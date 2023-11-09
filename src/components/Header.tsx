09ddae14ff1bf3738e61daab9b3293786146c7a5
João Bonifácio <joaojunior.b22@gmail.com>
2023-11-09 11:01:09 -0300

    header-semi-finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
-    <header className="bg-white shadow">
-        <nav className="container mx-auto px-4 py-6">
-            <h1 className="text-2xl font-bold text-gray-800">My Portfolio</h1>
+    <header className="bg-blue-700 shadow flex items-center w-[100%]">
+        <h1 className="text-2xl font-bold text-white w-[48%] ml-5">Portfolio do João</h1>
+        <nav className="container px-4 py-6 w-[48%] mr-5 flex justify-end">
+            <Link href="/">Home</Link>
+            <Link href="/sobre" className="ml-4">Sobre</Link>
+            <Link href="/contato" className="ml-4">Contato</Link>
         </nav>
     </header>
     ) 
