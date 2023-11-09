7341ff59dedb8a2a259511664d354a2bb9e3806d
João Bonifácio <joaojunior.b22@gmail.com>
2023-11-09 09:43:47 -0300

    portifolio3.0-init

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+    <footer className="bg-gray-200 py-4">
+        <div className="container mx-auto text-center text-gray-600">
+          &copy; 2023 João Júnior.
+        </div>
+    </footer>
+    ) 
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+    <header className="bg-white shadow">
+        <nav className="container mx-auto px-4 py-6">
+            <h1 className="text-2xl font-bold text-gray-800">My Portfolio</h1>
+        </nav>
+    </header>
+    ) 
+}
